a3127b02fbffaba56d8bdcd5ea16cdca86022b7b
Ruslan Kukharchuk <ruslan.kukharchuk@biqh.com>
2025-05-06 15:02:02 +0300

    First setting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,13 @@
 <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<ol>
+    <li>listItem1</li>
+    <li>listItem2</li>
+</ol>
+<ul>
+    <li>listItem1</li>
+    <li>listItem2</li>
+</ul>
+
+
